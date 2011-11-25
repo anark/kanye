4d009fc1c3edb7da6410a7b6618de094a7b889fc
Andrew Narkewicz <anark@corknine.com>
2011-11-24 18:07:51 -0800

    Skip tracks that have been deleted from the server

--- lib/kanye/track.rb
+++ lib/kanye/track.rb
@@ -20,22 +20,26 @@ module Kanye
   
     def download!
       raise(NoKeyError, "Couldn't find :key for '#{self}'") if key.blank?
-    
+
       response = HTTParty.get(url, :headers => {'cookie' => cookie})
-      raise "Response Code '#{response.code}' - Something has changed." unless response.code == 200
-    
-    
-      print "Attempting to download ", self
-      puts "\n\tDownloading song..."
-    
-      mp3_url = URI.escape(response.parsed_response["url"])
-      mp3_response = HTTParty.get(mp3_url)
-    
-      File.open(filename, "wb") do |f|
-        f.write(mp3_response.parsed_response)
+      if response.code == 404
+        puts "Cannot Download #{self}, Unavailable"
+      else
+        raise "Response Code '#{response.code}' - Something has changed." unless response.code == 200
+      
+      
+        print "Attempting to download ", self
+        puts "\n\tDownloading song..."
+      
+        mp3_url = URI.escape(response.parsed_response["url"])
+        mp3_response = HTTParty.get(mp3_url)
+      
+        File.open(filename, "wb") do |f|
+          f.write(mp3_response.parsed_response)
+        end
+      
+        set_id3_tags!
       end
-    
-      set_id3_tags!
     end
     
     def to_s
